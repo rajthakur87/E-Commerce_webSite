a1b07f91a827aad6a1e3ddd334ee7420c7693ae4
RajChauhan87 <23mca20517@cuchd.in>
2024-11-08 15:24:14 +0530

    commit

--- src/Compnents/CartItems/CartItems.jsx
+++ src/Compnents/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext';
 import remove_icon from '../Assets/cart_cross_icon.png';
 
 const CartItems = () => {
-    const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+    const { getTotalCartAmount,all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
     return (
         <div className="cartitems">
@@ -46,7 +46,7 @@ const CartItems = () => {
                     <div>
                         <div className="cartitems-total-item">
                             <p>Subtotal</p>
-                            <p>${0}</p>
+                            <p>${getTotalCartAmount()}</p>
                         </div>
                         <hr />
                         <div className="cartitems-total-item">
@@ -56,7 +56,7 @@ const CartItems = () => {
                         <hr />
                         <div className="cartitems-total-item">
                             <h3>Total</h3>
-                            <h3>${0}</h3>
+                            <h3>${getTotalCartAmount()}</h3>
                         </div>
                         <button>PROCEED TO CHECKOUT</button>
                     </div>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import './CSS/LoginSignup.css'
+import React from 'react';
+import './CSS/LoginSignup.css';
 
 const LoginSignup = () => {
+  
   return (
     <div className="loginsignup">
-        <div className="loginsignup-container">
-            <h1>Sign Up</h1>
-            <div className="loginsignup-fields">
-                <input type="text" placeholder='Your Name'/>
-                <input type="email" placeholder='Email Address' />
-                <input type="password" placeholder='Password' />
-            </div>
-            <button>Continue</button>
-            <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
-            <div className="loginsignup-agree">
-                <input type="checkbox" name='' id='' />
-                <p>By continuing, i agree to the terms of use & privacy policy.</p>
-            </div>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <form className="loginsignup-fields">
+          <input
+          type="text"placeholder="Your Name"/>
+          <input
+            type="email"placeholder="Email Address" />
+          <input
+            type="password"placeholder="Password"/>
+          <button type="submit">Continue</button>
+        </form>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input
+            type="checkbox"
+          />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
-
-
+export default LoginSignup;
